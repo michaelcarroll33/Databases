be289a56776d617d62a97324cb205401bc5d6865
michaelcarroll33 <michaelcarroll788@gmail.com>
2024-04-29 21:11:50 -0400

    Update HW2.sql

--- HW2.sql
+++ HW2.sql
@@ -5,11 +5,3 @@ CREATE TABLE publisher (
     website varchar2(50),
     primary key (Name)
 );
-CREATE TABLE book (
-    bookid varchar2(13) not null,
-    title varchar2(40) not null,
-    publicationdate varchar2(10) not null,
-    publishername varchar(15) not null,
-    primary key (bookid),
-    foreign key (publishername) references publisher(name)
-);
