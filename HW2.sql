5614ab0644104afea72b58735ef16e62a6bd7272
michaelcarroll33 <michaelcarroll788@gmail.com>
2024-04-29 21:29:44 -0400

    Update HW2.sql

--- HW2.sql
+++ HW2.sql
@@ -1,7 +1,35 @@
-CREATE TABLE publisher (
-    name varchar2(15) not null,
+CREATE TABLE customer (
+    Fname varchar2(15),
+    Lname varchar2(15),
+    ssn char(9),
     address varchar2(50),
-    phone varchar2(10),
-    website varchar2(50),
-    primary key (Name)
+    creditscore varchar2(3),
+    routingnumber varchar2(9),
+    primary key (ssn),
+    foreign key (routingnumber) references branch(routingnum)
+);
+
+CREATE TABLE account (
+    accountnum varchar2(10) not null,
+    interestrate numeric(4,3),
+    accounttype varchar2(10),
+    primary key (accountnum)
+);
+
+CREATE TABLE branch (
+    routingnum varchar2(9) not null,
+    cashbalance numeric(8,2),
+    city varchar2(10),
+    primary key (routingnum)
+);
+
+CREATE TABLE loan (
+    loannum varchar2(15),
+    loantype varchar2(15),
+    maturity numeric(2,0),
+    interestrate numeric(4,3),
+    amount numeric(8,2),
+    cssn varchar2(9),
+    primary key (loannum),
+    foreign key (cssn) references customer(ssn)
 );
